1366e55eb4025f0f911423466240858406af53ed
aimensasi <aimensasi@icloud.com>
2026-03-12 02:04:44 +0800

    Restructured project structure

---
+++ module_1_coordination/service_a/src/main/java/com/distributed_systems/module_1_coordination_service_a/CoordinationServiceA.java
@@ -0,0 +1,13 @@
+package com.distributed_systems.module_1_coordination_service_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinationServiceA {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordinationServiceA.class, args);
+	}
+
+}
---
+++ module_1_coordination/service_b/src/main/java/com/distributed_systems/module_1_coordination_service_b/CoordinationServiceB.java
@@ -0,0 +1,13 @@
+package com.distributed_systems.module_1_coordination_service_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinationServiceB {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordinationServiceB.class, args);
+	}
+
+}
